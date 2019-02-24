f35d54ba0d443a32015f3a6acb2c9e9755fcc623
aoa <amine.ouchiha@edifixio.com>
2019-02-24 11:50:23 +0100

    refactoring

---
+++ src/src/const.js
@@ -0,0 +1,2 @@
+const TEMPLATE_REGEXP = /(\{){2}[a-z|A-Z]{1}[a-z|A-Z|0-9|_|-]*(\.([a-z|A-Z]{1}[a-z|A-Z|0-9|_|-])*)*(\}){2}/g;
+const CURRENT_DIR = process.cwd();
---
+++ src/src/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require('path');
+
+
+
+
+
+
+
+console.log("refactoring ....")
+
+
+
