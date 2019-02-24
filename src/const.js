a0b1f35d9b821dad20d6bb4559285e070695af26
aoa <amine.ouchiha@edifixio.com>
2019-02-24 21:00:34 +0100

    modify regex to detect dot separated vars

--- src/const.js
+++ src/const.js
@@ -1,1 +1,1 @@
-exports.TEMPLATE_REGEXP = /(\{){2}[a-z|A-Z]{1}[a-z|A-Z|0-9|_|-]*(\.([a-z|A-Z]{1}[a-z|A-Z|0-9|_|-])*)*(\}){2}/g;
+exports.TEMPLATE_REGEXP = /(\{){2}[a-z|A-Z]{1}[a-z|A-Z|0-9|_|-]*((\.)[a-z|A-Z]{1}[a-z|A-Z|0-9|_|-]*)*(\}){2}/g;
