bfa29ac045109c9aa2518c21ac65c29e03e37d5d
aoa <amine.ouchiha@edifixio.com>
2019-02-24 21:00:54 +0100

    init index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-const msg = require('./const');
+
 const utils = require('./utils');
-const htmlCompiler = require('./htmlCompiler');
+const HtmlCompiler = require('./HtmlCompiler').HtmlCompiler;
 const path = require('path');
 
 const appRootDir = () => {
     let dirPathArray = process.cwd().split('/');
-    dirPathArray.pop();
     return dirPathArray.join('/');
 }
+
 const APP_ROOT_DIR = appRootDir();
 
 async function index() {
-    let html = utils.readFile(`${APP_ROOT_DIR}/demo/template/cv.html`);
-    console.log(appRootDir());
+    let html = (await utils.readFile(`${APP_ROOT_DIR}/demo/template/cv.html`)).toString();
+    let htmlCompiler = new HtmlCompiler(html);
+    console.log(htmlCompiler.findVarsInHtml());
 }
 
 index();
