064f918b9bb9ad17d5f8ce59dcb891de5c5d21ff
aoa <amine.ouchiha@edifixio.com>
2019-02-27 23:11:47 +0100

    copmpile simple vars and test methode

--- spec/HtmlCompiler.spec.js
+++ spec/HtmlCompiler.spec.js
@@ -14,22 +14,39 @@ let htmlCompiler = new HtmlCompiler(`<!DOCTYPE html>
       </body>
     </html>`);
 
+
+const compileResult = `<!DOCTYPE html>
+    <html lang="fr">
+      <head>
+        <meta charset="utf-8">
+        <title>Inscrire un titre ici</title>
+        <!-- On peut avoir d'autres méta-données ici -->
+      </head>
+      <body>
+          test1
+        <!-- Ici, on placera tout le contenu à destination 
+        de l'utilisateur -->
+        <div>test2</div>
+      </body>
+    </html>`;
+
 describe("htmlCompiler : html template processing and remplace with values", () => {
-  
+
   const vars = htmlCompiler.findVarsInHtml();
-  
+
   it(" htmlCompiler.findVarsInHtml : template vars found", () => {
     expect(vars).toEqual(['var1', 'var2.subvar1']);
   });
 
-  it("HtmlCompiler.accessToValueWithArrayOfFloorValue : using array of json floor to acces to valus", () => {
-    const jsonTest = {floor1 : { floor2 : 0 }, floor2:'a' }
-    expect(HtmlCompiler.accessToValueWithArrayOfFloorValue(['floor1', 'floor2'], jsonTest)).toBe(0);
-    expect(HtmlCompiler.accessToValueWithArrayOfFloorValue(['floor2'], jsonTest)).toBe('a');
-    expect(HtmlCompiler.accessToValueWithArrayOfFloorValue(['floor2', 'floor2'], jsonTest)).toBeUndefined();
+
+  it("mapTemplateVarsToConfigValues : map configuration to templating", () => {
+    const templateResult = htmlCompiler.compileSimpleValue({
+      'var1': 'test1', 'var2.subvar1': 'test2'
+    });
+    expect(templateResult).toEqual(compileResult);
   });
 
-  
+
 
 
 
--- src/HtmlCompiler.js
+++ src/HtmlCompiler.js
@@ -1,22 +1,31 @@
 const consts = require('./const');
 
 exports.HtmlCompiler = class HtmlCompiler {
-    
-    constructor(htmlTemplate){
+
+    constructor(htmlTemplate) {
         this.htmlTemplate = htmlTemplate;
     }
 
-    findVarsInHtml(){
-        return this.htmlTemplate.match(consts.TEMPLATE_REGEXP).map(tmpVar => tmpVar.substring(2, tmpVar.length - 2));
+    findVarsInHtml() {
+        return this.htmlTemplate
+                    .match(consts.TEMPLATE_REGEXP)
+                    .map(tmpVar => 
+                        tmpVar.substring(2, tmpVar.length - 2));
     }
 
-    static accessToValueWithArrayOfFloorValue(floorArray, json){
-        let curretVal = json;
-        floorArray.forEach(floorVar => {
-            if(!curretVal) return;
-            curretVal =  curretVal[floorVar];
-        });
-        return curretVal 
+
+    compileTemplate(simpleValueConfig, fileRefConfig){
+        this.compileSimpleValue(simpleValueConfig);
+        return this.htmlTemplate;
+
     }
-    
+
+    compileSimpleValue(simpleValueConfig){
+        const vars = this.findVarsInHtml();
+        vars.forEach(_var => {
+            this.htmlTemplate = this.htmlTemplate.replace("{{"+_var+"}}", simpleValueConfig[_var]);
+        });
+        return this.htmlTemplate; 
+    }
+
 }
