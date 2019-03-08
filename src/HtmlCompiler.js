7eee6c0d1f6c0a8d7a86603d8b5989cc0ab1e5f5
aoa <amine.ouchiha@edifixio.com>
2019-03-08 21:19:19 +0100

    add decoding URI refrence in template refrence

--- src/HtmlCompiler.js
+++ src/HtmlCompiler.js
@@ -1,11 +1,24 @@
 const consts = require('./const');
+const fs = require('fs');
 
+const TXT_EXENTION = ['svg'];
+const BINARY_EXTENTION = ['png','jpg','jpeg','gif'];
+
+/**
+ * 
+ */
 exports.HtmlCompiler = class HtmlCompiler {
-
+    /**
+     * 
+     * @param {*} htmlTemplate put html file
+     */
     constructor(htmlTemplate) {
         this.htmlTemplate = htmlTemplate;
     }
 
+    /**
+     * find template in variable
+     */
     findVarsInHtml() {
         return this.htmlTemplate
                     .match(consts.TEMPLATE_REGEXP)
@@ -13,19 +26,59 @@ exports.HtmlCompiler = class HtmlCompiler {
                         tmpVar.substring(2, tmpVar.length - 2));
     }
 
-
-    compileTemplate(simpleValueConfig, fileRefConfig){
+    /**
+     * remplace variable in template
+     * @param {*} simpleValueConfig simple value to remplce
+     * @param {*} fileRefConfig URI to remplace with base64 or with here text context
+     */
+    async compileTemplate(simpleValueConfig, fileRefConfig){
         this.compileSimpleValue(simpleValueConfig);
         return this.htmlTemplate;
 
     }
 
+
+    /**
+     * 
+     * @param {*} simpleValueConfig 
+     */
     compileSimpleValue(simpleValueConfig){
         const vars = this.findVarsInHtml();
         vars.forEach(_var => {
-            this.htmlTemplate = this.htmlTemplate.replace("{{"+_var+"}}", simpleValueConfig[_var]);
+            this.htmlTemplate = 
+                this.htmlTemplate.replace("{{"+_var+"}}", 
+                    simpleValueConfig[_var]);
         });
         return this.htmlTemplate; 
     }
+
+
+    /**
+     * 
+     * @param {*} fileRefConfig 
+     */
+    async compileFilesReferences(fileRefConfig){
+        const fileRefConfigKeys = Object.keys(fileRefConfig);
+        
+        await fileRefConfigKeys.forEach(async key => {
+            const fileURI = fileRefConfig[key];
+            const fileExt = val.split('.')[1];
+            try {
+                const fileBin = await fs.readFile(fileURI)
+                if(TXT_EXENTION.includes(fileExt)){
+                    fileRefConfig[key] = fileBin.toString();
+                }else if(BINARY_EXTENTION.includes(fileExt)){
+                    fileRefConfig[key] = new Buffer(fileBin).toString('base64');
+                }
+            } catch (error) {
+                throw error;
+            }
+        });
+
+        this.compileSimpleValue(fileRefConfig);
+        return this.htmlTemplate; 
+    }
+
+    
 
 }
