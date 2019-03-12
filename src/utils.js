e727b53d595317be91c3dd093be44774bff163bb
aoa <amine.ouchiha@edifixio.com>
2019-03-13 00:35:59 +0100

    convert to B64 / write files in fs

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-exports.readFile = (async function readFile(_path, isabsolute) {
-    return new Promise(function (resolve, reject) {
-        fs.readFile(_path, function (err, data) {
+exports.readFile = (async (_path, isabsolute) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(_path, (err, data) => {
             if (err) {
                 reject(err);
             }
@@ -10,3 +10,45 @@ exports.readFile = (async function readFile(_path, isabsolute) {
         });
     });
 });
+
+
+
+exports.writeFile = (async (_path, content) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(_path, content, (err) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    });
+});
+
+
+/**
+ * @author Amine OUCHIHA
+ * @param {*} html 
+ * @description convert html input to pdf using puppeteer
+ */
+exports.convert = async (html, _path, name, format) => {
+    try {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage();
+        await page.setContent(html);
+        page.emulateMedia('screen');
+        await page.pdf({
+            path: PDF_NAME,
+            format: PDF_FORMAT,
+            printBackground: true
+        });
+        await browser.close();
+        console.log('done');
+        process.exit();
+
+    } catch (e) {
+        console.log('our error', e);
+    }
+}
+
+
+
