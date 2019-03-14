e2b9816c65821a29a6881cc0fdf28fdc629aaca8
aoa <amine.ouchiha@edifixio.com>
2019-03-14 21:40:20 +0100

    add ENV VAR to avoid loop when using nodemon

--- src/index.js
+++ src/index.js
@@ -10,14 +10,16 @@ const appRootDir = () => {
 const APP_ROOT_DIR = appRootDir();
 
 async function index() {
-    let html = (await utils.readFile(`${APP_ROOT_DIR}/demo/template/cv.html`)).toString();
+    let html = (await utils.readFile(`${APP_ROOT_DIR}/demo/template/cv.hbs`)).toString();
     let htmlCompiler = new HtmlCompiler(html);
     const htmlResult = (await htmlCompiler.compileTemplate(
         {"var.text1" : "test1"},
-        {"var.img1" : `${APP_ROOT_DIR}/demo/assets/tr.png`}));
+        {"var.img1" : `${APP_ROOT_DIR}/demo/assets/tr.png`,
+    "var.style" : `${APP_ROOT_DIR}/demo/template/cv.css`}));
     await utils.writeFile(`${APP_ROOT_DIR}/output/cv.html`, htmlResult);
+    if(process.env.GEN_PDF){
+        utils.convertHtmlToPdf(htmlResult,`${APP_ROOT_DIR}/output/`, 'cv');
+    }
 }
 
 index();
-
-console.log("===>>> refactoring ....");
