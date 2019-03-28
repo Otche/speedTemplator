7aa07caea5af1d030b013d35825ba0ed758631b7
aoa <amine.ouchiha@edifixio.com>
2019-03-28 20:52:05 +0100

    aside template

--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ const appRootDir = () => {
 
 const APP_ROOT_DIR = appRootDir();
 
+const textTmplt = {"var.text1" : "test1"};
+const fileTmplt = {"file.cvImg" : `${APP_ROOT_DIR}/demo/assets/tr.png`,
+"file.style" : `${APP_ROOT_DIR}/demo/template/cv.css`,
+"file.knowlegeIcon" : `${APP_ROOT_DIR}/demo/assets/knowlege-3d.svg`,
+"file.localIcon" : `${APP_ROOT_DIR}/demo/assets/map-localization.svg`};
+
+
+
 async function index() {
     let html = (await utils.readFile(`${APP_ROOT_DIR}/demo/template/cv.hbs`)).toString();
     let htmlCompiler = new HtmlCompiler(html);
-    const htmlResult = (await htmlCompiler.compileTemplate(
-        {"var.text1" : "test1"},
-        {"var.img1" : `${APP_ROOT_DIR}/demo/assets/tr.png`,
-    "var.style" : `${APP_ROOT_DIR}/demo/template/cv.css`}));
+    const htmlResult = (await htmlCompiler.compileTemplate(textTmplt,fileTmplt));
     await utils.writeFile(`${APP_ROOT_DIR}/output/cv.html`, htmlResult);
     if(process.env.GEN_PDF){
         utils.convertHtmlToPdf(htmlResult,`${APP_ROOT_DIR}/output/`, 'cv');
